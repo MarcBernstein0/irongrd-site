af3fc85815b0980b4534eb57c0cb5035f5b60fda
Marc Bernstein <marcseth.bernstein@gmail.com>
2026-05-19 18:35:16 -0400

    restart

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/irongrid-site',
   plugins: [react()],
+  server: {
+    open: true,
+    port: 3001
+  }
 })
